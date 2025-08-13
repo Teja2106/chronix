7c959dc90e67965ad5fd71437947d1b9b32f3276
Teja2106 <teja182106@gmail.com>
2025-08-13 17:54:17 +0530

    Revert "added clerk authentication, along with made changes to the UI by making the To-Do container and keybinds container as bento box layout."
    This reverts commit 6b1d5344e32542a4f2044672a17e7b1576f10bb7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
-import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>{children}</ClerkProvider>
-        <Toaster position="top-center" theme="dark"/>
+        {children}
       </body>
     </html>
   );
